dba10635850fc262e32a1b15ba1502498f929d80
Ömer Kaan Coşkun <75243197+KaanOmeer@users.noreply.github.com>
2021-03-24 10:34:47 +0300

    Delete dersler/ders7 directory

--- dersler/ders7/break.py
+++
@@ -1,6 +0,0 @@
-a = 0
-while (a < 60):
-    print(a)
-    if (a==15):
-        break
-    a +=1
--- dersler/ders7/teksayıyazma.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Başlangıç:"))
-b=int(input("bitiş:"))
-i=a
-while i<b:
-    i+=1
-    if(i%2==1):
-        print(i)
